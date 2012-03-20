6beb6aec5c6fbd48c8c4e2e0633e2870da194318
Peter Chase <cchasep@hotmail.com>
2012-03-20 18:18:30 -0300

    Added task details data source

---
+++ TaskManager2/Classes/Tasks/TaskDisplay/TaskDataSources/TaskDetailsDataSource.h
@@ -0,0 +1,13 @@
+//
+//  TaskDetailsDataSource.h
+//  TaskManager2
+//
+//  Created by Peter Chase on 12-03-20.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TaskDetailsDataSource : NSObject<UITableViewDataSource>
+
+@end
---
+++ TaskManager2/Classes/Tasks/TaskDisplay/TaskDataSources/TaskDetailsDataSource.m
@@ -0,0 +1,13 @@
+//
+//  TaskDetailsDataSource.m
+//  TaskManager2
+//
+//  Created by Peter Chase on 12-03-20.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TaskDetailsDataSource.h"
+
+@implementation TaskDetailsDataSource
+
+@end
